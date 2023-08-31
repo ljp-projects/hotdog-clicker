df58c0bbe4c0ffb28f7d5b21d9d4299288701400
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-01 08:04:27 +1000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -40,35 +40,6 @@ const dogFarmButton = document.getElementById("dogFarmButton");
 const facButton = document.getElementById("dogFacButton");
 const bankButton = document.getElementById("dogBankButton")
 
-const save = () => {
-    window.sessionStorage.setItem("Clicks", clickCount)
-    window.sessionStorage.setItem("Passive", passiveClicks)
-    window.sessionStorage.setItem("Buns", bunCount)
-    window.sessionStorage.setItem("Dads", dadCount)
-    window.sessionStorage.setItem("Grills", grillCount)
-    window.sessionStorage.setItem("Farms", dogFarmCount)
-    window.sessionStorage.setItem("Factories", facCount)
-    window.sessionStorage.setItem("Banks", bankCount)
-}
-
-window.addEventListener("beforeunload", () => {
-    alert("Saving the game...")
-    save();
-});
-
-const load = () => {
-    clickCount = window.sessionStorage.getItem("Clicks")
-    passiveClicks = window.sessionStorage.getItem("Passive")
-    bunCount = window.sessionStorage.getItem("Buns")
-    dadCount = window.sessionStorage.getItem("Dads")
-    grillCount = window.sessionStorage.getItem("Grills")
-    dogFarmCount = window.sessionStorage.getItem("Farms")
-    facCount = window.sessionStorage.getItem("Factories")
-    bankCount = window.sessionStorage.getItem("Banks")
-}
-
-load()
-
 hotdogButton.addEventListener("click", function() {
     clickCount++;
     clickCountElement.textContent = clickCount.toFixed(1);
@@ -202,3 +173,32 @@ crispButton.addEventListener("click", function() {
         bunRate *= 2;
     }
 });
+
+const save = () => {
+    window.sessionStorage.setItem("Clicks", clickCount)
+    window.sessionStorage.setItem("Passive", passiveClicks)
+    window.sessionStorage.setItem("Buns", bunCount)
+    window.sessionStorage.setItem("Dads", dadCount)
+    window.sessionStorage.setItem("Grills", grillCount)
+    window.sessionStorage.setItem("Farms", dogFarmCount)
+    window.sessionStorage.setItem("Factories", facCount)
+    window.sessionStorage.setItem("Banks", bankCount)
+}
+
+window.addEventListener("beforeunload", () => {
+    alert("Saving the game...")
+    save();
+});
+
+const load = () => {
+    window.sessionStorage.getItem("Clicks") != null ? clickCount = window.sessionStorage.getItem("Clicks"); clickCountElement.textContent = clickCount.toFixed(1) : console.log("is null")
+    window.sessionStorage.getItem("Passive") != null ? passiveClicks = window.sessionStorage.getItem("Passive"); passiveClicksElement.textContent = passiveClicks.toFixed(1) : console.log("is null")
+    window.sessionStorage.getItem("Buns") != null ? bunCount = window.sessionStorage.getItem("Buns"); bunCountElement.textContent = bunCount : console.log("is null")
+    window.sessionStorage.getItem("Dads") != null ? dadCount = window.sessionStorage.getItem("Dads"); dadCountElement.textContent = dadCount : console.log("is null")
+    window.sessionStorage.getItem("Grills") != null ? grillCount = window.sessionStorage.getItem("Grills"); grillCountElement.textContent = grillCount : console.log("is null")
+    window.sessionStorage.getItem("Farms") != null ? dogFarmCount = window.sessionStorage.getItem("Farms"); dogFarmCountElement.textContent = dogFarmCount : console.log("is null")
+    window.sessionStorage.getItem("Factories") != null ? facCount = window.sessionStorage.getItem("Factories"); facCountElement.textContent = facCount : console.log("is null")
+    window.sessionStorage.getItem("Banks") != null ? bankCount = window.sessionStorage.getItem("Banks"); bankCountElement.textContent = bankCount : console.log("is null")
+}
+
+load()
