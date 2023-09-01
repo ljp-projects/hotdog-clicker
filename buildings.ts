794e7561eafcf3002f525544a1141ad29f2ae356
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-02 07:00:12 +1000

    ts

--- buildings.ts
+++ buildings.ts
@@ -1,61 +1,74 @@
-var grill = new Audio("designed-fire-winds-swoosh-04-116788.mp3");
-var bun = new Audio("crunch-crispy-breadbun-41340.mp3");
-var dad = new Audio("dad-says-yummy-113126.mp3");
-var farm = new Audio("zapsplat_impacts_hit_loose_ground_dirt_impact_collapse_fall_013_102682.mp3");
-var factory = new Audio("some-kind-of-machine-103152.mp3");
-var bank = new Audio("cash-register-purchase-87313.mp3");
-var passiveClicks = 0;
-var clickCount = 0;
-var bunCount = 0;
-var dadCount = 0;
-var grillCount = 0;
-var dogFarmCount = 0;
-var facCount = 0;
-var bankCount = 0;
-var bunCost = 10;
-var bunRate = 0.2;
-var dadCost = 100;
-var dadRate = 2;
-var grillCost = 500;
-var grillRate = 10;
-var dogFarmCost = 5000;
-var dogFarmRate = 50;
-var facCost = 50000;
-var facRate = 500;
-var bankCost = 250000;
-var bankRate = 2500;
-var increment = 1.3;
-var passiveClicksElement = document.getElementById("passive");
-var clickCountElement = document.getElementById("clickCount");
-var grillCountElement = document.getElementById("grillCount");
-var bunCountElement = document.getElementById("bunCount");
-var dadCountElement = document.getElementById("dadCount");
-var dogFarmCountElement = document.getElementById("dogFarmCount");
-var facCountElement = document.getElementById("dogFacCount");
-var bankCountElement = document.getElementById("dogBankCount");
-var hotdogButton = document.getElementById("hotdogButton");
-var bunButton = document.getElementById("bunButton");
-var dadButton = document.getElementById("dadButton");
-var grillButton = document.getElementById("grillButton");
-var dogFarmButton = document.getElementById("dogFarmButton");
-var facButton = document.getElementById("dogFacButton");
-var bankButton = document.getElementById("dogBankButton");
-var bunPriceElement = document.getElementById("bunPrice");
-var dadPriceElement = document.getElementById("dadPrice");
-var grillPriceElement = document.getElementById("grillPrice");
-var farmPriceElement = document.getElementById("farmPrice");
-var facPriceElement = document.getElementById("facPrice");
-var bankPriceElement = document.getElementById("bankPrice");
-hotdogButton === null || hotdogButton === void 0 ? void 0 : hotdogButton.addEventListener("click", function () {
+var grill: HTMLAudioElement | null = new Audio("designed-fire-winds-swoosh-04-116788.mp3")
+var bun: HTMLAudioElement | null = new Audio("crunch-crispy-breadbun-41340.mp3")
+var dad: HTMLAudioElement | null = new Audio("dad-says-yummy-113126.mp3")
+var farm: HTMLAudioElement | null = new Audio("zapsplat_impacts_hit_loose_ground_dirt_impact_collapse_fall_013_102682.mp3")
+var factory: HTMLAudioElement | null = new Audio("some-kind-of-machine-103152.mp3")
+var bank: HTMLAudioElement | null = new Audio("cash-register-purchase-87313.mp3")
+
+let passiveClicks: number = 0;
+let clickCount: number = 0;
+let bunCount: number = 0;
+let dadCount: number = 0;
+let grillCount: number = 0;
+let dogFarmCount: number = 0;
+let facCount: number = 0;
+let bankCount: number = 0;
+
+let bunCost: number = 10;
+let bunRate: number = 0.2;
+
+let dadCost: number = 100;
+let dadRate: number = 2;
+
+let grillCost: number = 500;
+let grillRate: number = 10;
+
+let dogFarmCost: number = 5000;
+let dogFarmRate: number = 50;
+
+let facCost: number = 50000;
+let facRate: number = 500;
+
+let bankCost: number = 250000;
+let bankRate: number = 2500;
+
+const increment: number = 1.3;
+
+const passiveClicksElement: HTMLElement | null = document.getElementById("passive");
+const clickCountElement: HTMLElement | null = document.getElementById("clickCount");
+
+const grillCountElement: HTMLElement | null = document.getElementById("grillCount");
+const bunCountElement: HTMLElement | null = document.getElementById("bunCount");
+const dadCountElement: HTMLElement | null = document.getElementById("dadCount");
+const dogFarmCountElement: HTMLElement | null = document.getElementById("dogFarmCount");
+const facCountElement: HTMLElement | null = document.getElementById("dogFacCount");
+const bankCountElement: HTMLElement | null = document.getElementById("dogBankCount");
+
+const hotdogButton: HTMLElement | null = document.getElementById("hotdogButton");
+const bunButton: HTMLElement | null = document.getElementById("bunButton");
+const dadButton: HTMLElement | null = document.getElementById("dadButton");
+const grillButton: HTMLElement | null = document.getElementById("grillButton");
+const dogFarmButton: HTMLElement | null = document.getElementById("dogFarmButton");
+const facButton: HTMLElement | null = document.getElementById("dogFacButton");
+const bankButton: HTMLElement | null = document.getElementById("dogBankButton")
+
+const bunPriceElement: HTMLElement | null = document.getElementById("bunPrice")
+const dadPriceElement: HTMLElement | null = document.getElementById("dadPrice")
+const grillPriceElement: HTMLElement | null = document.getElementById("grillPrice")
+const farmPriceElement: HTMLElement | null = document.getElementById("farmPrice")
+const facPriceElement: HTMLElement | null = document.getElementById("facPrice")
+const bankPriceElement: HTMLElement | null = document.getElementById("bankPrice")
+
+hotdogButton?.addEventListener("click", function() {
     if (clickCountElement != null) {
         clickCount++;
         clickCountElement.textContent = clickCount.toFixed(1);
-    }
-    else {
-        console.log("Could not find clickCountElement");
+    } else {
+        console.log("Could not find clickCountElement")
     }
 });
-bunButton === null || bunButton === void 0 ? void 0 : bunButton.addEventListener("click", function () {
+
+bunButton?.addEventListener("click", function() {
     if (clickCount >= bunCost && bunPriceElement != null && clickCountElement != null && bunCountElement != null && passiveClicksElement != null) {
         clickCount -= bunCost;
         bunCost *= increment;
@@ -65,10 +78,10 @@ bunButton === null || bunButton === void 0 ? void 0 : bunButton.addEventListener
         bunCountElement.textContent = String(bunCount);
         passiveClicks += bunRate;
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
-        bun === null || bun === void 0 ? void 0 : bun.play();
+        bun?.play();
     }
 });
-dadButton === null || dadButton === void 0 ? void 0 : dadButton.addEventListener("click", function () {
+dadButton?.addEventListener("click", function() {
     if (clickCount >= dadCost && dadPriceElement != null && clickCountElement != null && dadCountElement != null && passiveClicksElement != null) {
         clickCount -= dadCost;
         dadCost *= increment;
@@ -78,10 +91,10 @@ dadButton === null || dadButton === void 0 ? void 0 : dadButton.addEventListener
         dadCountElement.textContent = String(dadCount);
         passiveClicks += dadRate;
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
-        dad === null || dad === void 0 ? void 0 : dad.play();
+        dad?.play();
     }
 });
-grillButton === null || grillButton === void 0 ? void 0 : grillButton.addEventListener("click", function () {
+grillButton?.addEventListener("click", function() {
     if (clickCount >= grillCost && grillPriceElement != null && clickCountElement != null && grillCountElement != null && passiveClicksElement != null) {
         clickCount -= grillCost;
         grillCost *= increment;
@@ -91,10 +104,10 @@ grillButton === null || grillButton === void 0 ? void 0 : grillButton.addEventLi
         grillCountElement.textContent = String(grillCount);
         passiveClicks += grillRate;
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
-        grill === null || grill === void 0 ? void 0 : grill.play();
+        grill?.play();
     }
 });
-dogFarmButton === null || dogFarmButton === void 0 ? void 0 : dogFarmButton.addEventListener("click", function () {
+dogFarmButton?.addEventListener("click", function() {
     if (clickCount >= dogFarmCost && farmPriceElement != null && clickCountElement != null && dogFarmCountElement != null && passiveClicksElement != null) {
         clickCount -= dogFarmCost;
         dogFarmCost *= increment;
@@ -104,10 +117,10 @@ dogFarmButton === null || dogFarmButton === void 0 ? void 0 : dogFarmButton.addE
         dogFarmCountElement.textContent = String(dogFarmCount);
         passiveClicks += dogFarmRate;
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
-        farm === null || farm === void 0 ? void 0 : farm.play();
+        farm?.play();
     }
 });
-facButton === null || facButton === void 0 ? void 0 : facButton.addEventListener("click", function () {
+facButton?.addEventListener("click", function() {
     if (clickCount >= facCost && facPriceElement != null && clickCountElement != null && facCountElement != null && passiveClicksElement != null) {
         clickCount -= facCost;
         facCost *= increment;
@@ -117,10 +130,10 @@ facButton === null || facButton === void 0 ? void 0 : facButton.addEventListener
         facCountElement.textContent = String(facCount);
         passiveClicks += facRate;
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
-        factory === null || factory === void 0 ? void 0 : factory.play();
+        factory?.play();
     }
 });
-bankButton === null || bankButton === void 0 ? void 0 : bankButton.addEventListener("click", function () {
+bankButton?.addEventListener("click", function() {
     if (clickCount >= bankCost && bankPriceElement != null && clickCountElement != null && bankCountElement != null && passiveClicksElement != null) {
         clickCount -= bankCost;
         bankCost *= increment;
@@ -130,17 +143,21 @@ bankButton === null || bankButton === void 0 ? void 0 : bankButton.addEventListe
         bankCountElement.textContent = String(bankCount);
         passiveClicks += bankRate;
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
-        bank === null || bank === void 0 ? void 0 : bank.play();
+        bank?.play();
     }
 });
+
 // Upgrades
-var mustardCost = 5000;
-var tSauceCost = 1000;
-var crispCost = 5000;
-var tSauceButton = document.getElementById("tSauceButton");
-var mustardButton = document.getElementById("mustardButton");
-var crispButton = document.getElementById("crispButton");
-tSauceButton === null || tSauceButton === void 0 ? void 0 : tSauceButton.addEventListener("click", function () {
+
+const mustardCost = 5000;
+const tSauceCost = 1000;
+const crispCost = 5000;
+
+const tSauceButton = document.getElementById("tSauceButton");
+const mustardButton = document.getElementById("mustardButton");
+const crispButton = document.getElementById("crispButton");
+
+tSauceButton?.addEventListener("click", function() {
     if (clickCount >= tSauceCost && clickCountElement != null && passiveClicksElement != null) {
         clickCount -= tSauceCost;
         clickCountElement.textContent = clickCount.toFixed(1);
@@ -149,7 +166,8 @@ tSauceButton === null || tSauceButton === void 0 ? void 0 : tSauceButton.addEven
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
     }
 });
-mustardButton === null || mustardButton === void 0 ? void 0 : mustardButton.addEventListener("click", function () {
+
+mustardButton?.addEventListener("click", function() {
     if (clickCount >= mustardCost && clickCountElement != null && passiveClicksElement != null) {
         clickCount -= mustardCost;
         clickCountElement.textContent = clickCount.toFixed(1);
@@ -158,7 +176,8 @@ mustardButton === null || mustardButton === void 0 ? void 0 : mustardButton.addE
         passiveClicksElement.textContent = passiveClicks.toFixed(1);
     }
 });
-crispButton === null || crispButton === void 0 ? void 0 : crispButton.addEventListener("click", function () {
+
+crispButton?.addEventListener("click", function() {
     if (clickCount >= crispCost && clickCountElement != null) {
         clickCount -= crispCost;
         clickCountElement.textContent = clickCount.toFixed(1);
@@ -166,7 +185,8 @@ crispButton === null || crispButton === void 0 ? void 0 : crispButton.addEventLi
         bunRate *= 2;
     }
 });
-setInterval(function () {
+
+setInterval(function() {
     if (clickCountElement != null) {
         clickCount += passiveClicks;
         clickCountElement.textContent = clickCount.toFixed(1);
---
+++ old/buildings.js
@@ -0,0 +1,175 @@
+var grill = new Audio("designed-fire-winds-swoosh-04-116788.mp3")
+var bun = new Audio("crunch-crispy-breadbun-41340.mp3")
+var dad = new Audio("dad-says-yummy-113126.mp3")
+var farm = new Audio("zapsplat_impacts_hit_loose_ground_dirt_impact_collapse_fall_013_102682.mp3")
+var factory = new Audio("some-kind-of-machine-103152.mp3")
+var bank = new Audio("cash-register-purchase-87313.mp3")
+let passiveClicks = Number(0);
+let clickCount = Number(0);
+let bunCount = 0;
+let dadCount = 0;
+let grillCount = 0;
+let dogFarmCount = 0;
+let facCount = 0;
+let bankCount = 0;
+let bunCost = 10;
+let bunRate = 0.2;
+let dadCost = 100;
+let dadRate = 2;
+let grillCost = 500;
+let grillRate = 10;
+let dogFarmCost = 5000;
+let dogFarmRate = 50;
+let facCost = 50000;
+let facRate = 500;
+let bankCost = 250000;
+let bankRate = 2500;
+const increment = 1.3;
+const clickCountElement = document.getElementById("clickCount");
+const grillCountElement = document.getElementById("grillCount");
+const passiveClicksElement = document.getElementById("passive");
+const bunCountElement = document.getElementById("bunCount");
+const dadCountElement = document.getElementById("dadCount");
+const dogFarmCountElement = document.getElementById("dogFarmCount");
+const facCountElement = document.getElementById("dogFacCount");
+const bankCountElement = document.getElementById("dogBankCount")
+const hotdogButton = document.getElementById("hotdogButton");
+const bunButton = document.getElementById("bunButton");
+const dadButton = document.getElementById("dadButton");
+const grillButton = document.getElementById("grillButton");
+const dogFarmButton = document.getElementById("dogFarmButton");
+const facButton = document.getElementById("dogFacButton");
+const bankButton = document.getElementById("dogBankButton")
+const bunPriceElement = document.getElementById("bunPrice")
+const dadPriceElement = document.getElementById("dadPrice")
+const grillPriceElement = document.getElementById("grillPrice")
+const farmPriceElement = document.getElementById("farmPrice")
+const facPriceElement = document.getElementById("facPrice")
+const bankPriceElement = document.getElementById("bankPrice")
+
+hotdogButton.addEventListener("click", function() {
+    clickCount++;
+    clickCountElement.textContent = clickCount.toFixed(1);
+});
+
+bunButton.addEventListener("click", function() {
+    if (clickCount >= bunCost) {
+        clickCount -= bunCost;
+        bunCost *= increment;
+        bunPriceElement.textContent = bunCost;
+        clickCountElement.textContent = clickCount;
+        bunCount++;
+        bunCountElement.textContent = bunCount;
+        passiveClicks += bunRate;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+        bun.play();
+    }
+});
+dadButton.addEventListener("click", function() {
+    if (clickCount >= dadCost) {
+        clickCount -= dadCost;
+        dadCost *= increment;
+        dadPriceElement.textContent = dadCost;
+        clickCountElement.textContent = clickCount;
+        dadCount++;
+        dadCountElement.textContent = dadCount;
+        passiveClicks += dadRate;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+        dad.play();
+    }
+});
+grillButton.addEventListener("click", function() {
+    if (clickCount >= grillCost) {
+        clickCount -= grillCost;
+        grillCost *= increment;
+        grillPriceElement.textContent = grillCost;
+        clickCountElement.textContent = clickCount;
+        grillCount++;
+        grillCountElement.textContent = grillCount;
+        passiveClicks += grillRate;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+        grill.play();
+    }
+});
+dogFarmButton.addEventListener("click", function() {
+    if (clickCount >= dogFarmCost) {
+        clickCount -= dogFarmCost;
+        dogFarmCost *= increment;
+        farmPriceElement.textContent = dogFarmCost;
+        clickCountElement.textContent = clickCount;
+        dogFarmCount++;
+        dogFarmCountElement.textContent = dogFarmCount;
+        passiveClicks += dogFarmRate;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+        farm.play();
+    }
+});
+facButton.addEventListener("click", function() {
+    if (clickCount >= facCost) {
+        clickCount -= facCost;
+        facCost *= increment;
+        facPriceElement.textContent = facCost;
+        clickCountElement.textContent = clickCount;
+        facCount++;
+        facCountElement.textContent = facCount;
+        passiveClicks += facRate;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+        factory.play();
+    }
+});
+bankButton.addEventListener("click", function() {
+    if (clickCount >= bankCost) {
+        clickCount -= bankCost;
+        bankCost *= increment;
+        bankPriceElement.textContent = bankCost;
+        clickCountElement.textContent = clickCount;
+        bankCount++;
+        bankCountElement.textContent = bankCount;
+        passiveClicks += bankRate;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+        bank.play();
+    }
+});
+
+// Upgrades
+
+const mustardCost = 5000;
+const tSauceCost = 1000;
+const crispCost = 5000;
+const tSauceButton = document.getElementById("tSauceButton");
+const mustardButton = document.getElementById("mustardButton");
+const crispButton = document.getElementById("crispButton");
+
+tSauceButton.addEventListener("click", function() {
+    if (clickCount >= tSauceCost) {
+        clickCount -= tSauceCost;
+        clickCountElement.textContent = clickCount.toFixed(1);
+        tSauceButton.style.display = "none";
+        passiveClicks += 25;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+    }
+});
+
+mustardButton.addEventListener("click", function() {
+    if (clickCount >= mustardCost) {
+        clickCount -= mustardCost;
+        clickCountElement.textContent = clickCount.toFixed(1);
+        mustardButton.style.display = "none";
+        passiveClicks += 50;
+        passiveClicksElement.textContent = passiveClicks.toFixed(1);
+    }
+});
+
+crispButton.addEventListener("click", function() {
+    if (clickCount >= crispCost) {
+        clickCount -= crispCost;
+        clickCountElement.textContent = clickCount.toFixed(1);
+        crispButton.style.display = "none";
+        bunRate *= 2;
+    }
+});
+
+setInterval(function() {
+    clickCount += passiveClicks;
+    clickCountElement.textContent = clickCount.toFixed(1);
+}, 1000);
